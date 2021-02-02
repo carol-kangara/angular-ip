eb6f794aa92b309e7e46c689a46949beafd47e12
carol-kangara <carolkangara30@gmail.com>
2021-02-02 07:01:16 +0300

    add quote component html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { GoalFormComponent } from './goal-form/goal-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent
+    QuotesComponent,
+    GoalFormComponent
   ],
   imports: [
     BrowserModule,
