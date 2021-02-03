a175323ced0d6a3aea1bf51f6a21f482aab00eaa
carol-kangara <carolkangara30@gmail.com>
2021-02-03 05:27:50 +0300

    quote-detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuotesFormComponent } from './quotes-form/quotes-form.component';
 import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
     QuotesFormComponent,
-    StrikethroughDirective
+    StrikethroughDirective,
+    DateCountPipe,
+    QuoteDetailComponent
   ],
   imports: [
     BrowserModule,
--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -2,5 +2,5 @@ export class Quotes {
     id!:number;
     quote!: string;
     author!: string;
-    Date!: Date;
+    completeDate!: Date;
 }
